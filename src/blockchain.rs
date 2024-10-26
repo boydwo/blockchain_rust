2a0b9d53ccc4735a8c9e2b2045f5032feb6e4dc5
Marcos Tulio Rodrigues <marcostulio06@hotmail.com>
2024-10-26 16:06:37 -0300

    feat: implements blockchain with database

---
+++ src/blockchain.rs
@@ -0,0 +1,92 @@
+pub mod blockchain {
+    use blockchain::block::Block;
+    use log::info;
+
+    pub type Result<T> = std::result::Result<T, failure::Error>;
+
+    const TARGET_HEXS: usize = 4;
+
+    #[derive(Clone, Debug)]
+    pub struct Blockchain {
+        current_hash: String,
+        db: sled::Db,
+    }
+
+    pub struct BlockchainIterator<'a> {
+        current_hash: String,
+        bc: &'a Blockchain,
+    }
+
+    impl Blockchain {
+        pub fn new() -> Result<Blockchain> {
+            info!("Open Blockchain");
+
+            let db = sled::open("data/blocks")?;
+            match db.get("LAST")? {
+                Some(hash) => {
+                    let last_hash = String::from_utf8(hash.to_vec())?;
+
+                    Ok(Blockchain {
+                        current_hash: last_hash,
+                        db,
+                    })
+                }
+                None => {
+                    let block = Block::new_genesis_block();
+                    db.insert(block.get_hash(), bincode::serialize(&block)?)?;
+                    db.insert("LAST", block.get_hash().as_bytes())?;
+
+                    let bc = Blockchain {
+                        current_hash: block.get_hash(),
+                        db,
+                    };
+
+                    bc.db.flush()?;
+                    Ok(bc)
+                }
+            }
+        }
+
+        pub fn add_block(&mut self, data: String) -> Result<()> {
+            let last_hash = self.db.get("LAST")?.unwrap();
+
+            let new_block =
+                Block::new_block(data, String::from_utf8(last_hash.to_vec())?, TARGET_HEXS)?;
+
+            self.db
+                .insert(new_block.get_hash(), bincode::serialize(&new_block)?)?;
+            self.db.insert("LAST", new_block.get_hash().as_bytes())?;
+
+            self.current_hash = new_block.get_hash();
+            Ok(())
+        }
+
+        pub fn inter(&self) -> BlockchainIterator {
+            BlockchainIterator {
+                current_hash: self.current_hash.clone(),
+                bc: &self,
+            }
+        }
+    }
+
+    impl<'a> Iterator for BlockchainIterator<'a> {
+        type Item = Block;
+
+        fn next(&mut self) -> Option<Self::Item> {
+            if let Ok(encode_block) = self.bc.db.get(&self.current_hash) {
+                return match encode_block {
+                    Some(b) => {
+                        if let Ok(block) = bincode::deserialize::<Block>(&b) {
+                            self.current_hash = block.get_prev_hash();
+                            Some(block)
+                        } else {
+                            None
+                        }
+                    }
+                    None => None,
+                };
+            }
+            None
+        }
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,18 @@
-use block::Blockchain;
+use blockchain::blockchain::Blockchain;
 
-mod block;
+mod blockchain;
 
-fn main() {
-    let mut b = Blockchain::new();
+pub type Result<T> = std::result::Result<T, failure::Error>;
+
+fn main() -> Result<()> {
+    let mut b = Blockchain::new()?;
     let _ = b.add_block("Send 1 BTC to Ivan, data1".to_string());
+    let _ = b.add_block("Send 2 more BTC to Ivan, data2".to_string());
+    let _ = b.add_block("Send 3 more BTC to Ivan, data2".to_string());
 
-    dbg!(&b);
+    for block in b.inter() {
+        println!("{:?}", block);
+    }
+
+    Ok(())
 }
